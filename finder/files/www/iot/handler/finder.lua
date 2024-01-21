10c45942f9507976c7c93a488b3015870daa5124
shegaoyuan@aliyun.com <shegaoyuan@aliyun.com>
2024-01-21 10:17:33 +0800

    add log in lua script

--- finder/files/www/iot/handler/finder.lua
+++ finder/files/www/iot/handler/finder.lua
@@ -1,6 +1,7 @@
 local cjson = require 'cjson.safe'
 local uci = require 'uci'
 local fs = require 'iot.fs'
+local log = require 'iot.log'
 
 local M = {}
 
@@ -8,9 +9,10 @@ local status_file = '/tmp/iot/iot-agent.status'
 --params: string, response from broadcaster
 --return: string or none
 M.on_message = function (params)
+    log.debug(params)
     local req = cjson.decode(params)
-    --log.debug(req.data.address)
     if req == nil or req.data == nil or type(req.data.address) ~= 'string' then
+        log.error('invalid request, address is nil or not string')
         return
     end
 
@@ -19,6 +21,7 @@ M.on_message = function (params)
     if agent ~= nil and agent.controller ~= req.data.address then
         c:set('agent', 'global', 'controller', req.data.address)
         c:commit('agent')
+        log.debug('controller changed to '..req.data.address)
         os.execute('/etc/init.d/iot-agent restart')
     else
         local proto = agent.proto or ''
@@ -28,6 +31,7 @@ M.on_message = function (params)
             local status = cjson.decode(data)
             if status ~= nil and status.state == 'disconnected' then
                 if proto..'://'..req.data.address..':'..port ~= status.address then
+                    log.debug('controller changed to '..req.data.address)
                     os.execute('/etc/init.d/iot-agent restart')
                 end
             end
--- iot-agent/files/www/iot/handler/iot-agent.lua
+++ iot-agent/files/www/iot/handler/iot-agent.lua
@@ -1,5 +1,6 @@
 local cjson = require 'cjson.safe'
 local fs = require 'iot.fs'
+local log = require 'iot.log'
 
 local M = {}
 
@@ -8,16 +9,20 @@ local status_file = status_dir .. 'iot-agent.status'
 --params: string, event msg
 --return: none
 M.on_event = function (params)
+    log.debug(params)
     local msg = cjson.decode(params)
     if msg == nil or msg.event == nil or type(msg.event) ~= 'string' then
+        log.error('invalid request, event is nil or not string')
         return
     end
     if msg.event ~= 'connected' and msg.event ~= 'disconnected' then
+        log.error('invalid request, event is not connected or disconnected')
         return
     end
     os.execute('mkdir -p ' .. status_dir)
     fs.writefile(status_file, cjson.encode({state = msg.event, address = msg.address}))
     if msg.event == 'disconnected' then
+        log.debug('send USR1 to finder')
         os.execute('kill -USR1 `pidof finder`')
     end
 end
