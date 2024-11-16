d5262b5b68ac12e29c8675d30ddf8335e1f32cc7
shegaoyuan@aliyun.com <shegaoyuan@aliyun.com>
2024-11-16 16:06:41 +0800

    feat: update finder

--- finder/files/www/iot/handler/finder.lua
+++ finder/files/www/iot/handler/finder.lua
@@ -1,45 +1,102 @@
 local cjson = require 'cjson.safe'
 local uci = require 'uci'
-local fs = require 'iot.fs'
-local log = require 'iot.log'
 
 local M = {}
 
-local status_file = '/tmp/iot/iot-agent.status'
---params: string, response from broadcaster
---return: string or none
-M.on_message = function (params)
-    log.debug(params)
-    local req = cjson.decode(params)
-    if req == nil or req.data == nil or type(req.data.address) ~= 'string' then
-        log.error('invalid request, address is nil or not string')
-        return
-    end
+local function discovery(req)
+
+    local inet_prefix = "inet addr:"
+
+    local address = req.address
 
     local c = uci.cursor()
-    local agent = c:get_all('agent', 'global')
-    if agent ~= nil and agent.controller ~= req.data.address then
-        c:set('agent', 'global', 'controller', req.data.address)
-        c:commit('agent')
-        log.debug('controller changed to '..req.data.address)
-        os.execute('/etc/init.d/iot-agent restart')
-    else
-        local proto = agent.proto or ''
-        local port = agent.port or ''
-        local data = fs.readfile(status_file)
-        if data ~= nil then
-            local status = cjson.decode(data)
-            if status ~= nil and status.state == 'disconnected' then
-                if proto..'://'..req.data.address..':'..port ~= status.address then
-                    log.debug('controller changed to '..req.data.address)
-                    os.execute('/etc/init.d/iot-agent restart')
-                end
+    local res = c:get_all('broadcaster', 'controller')
+    if res == nil or res.type == nil or res.type ~= 'static' or res.address == nil or res.address == "" then
+        -- auto
+        local cmd = "ifconfig | grep " .. address
+        local handle = io.popen(cmd)
+        if handle == nil then
+            return cjson.encode({
+                code = -1,
+                type = 'offer',
+                message = 'failed to get address'
+            })
+        end
+        local result = handle:read("*a")
+        handle:close()
+
+        local lines = {}
+        for line in string.gmatch(result, "[^\r\n]+") do
+            table.insert(lines, line)
+        end
+        for _, line in pairs(lines) do
+            if string.find(line, inet_prefix) then
+                local start_pos = string.find(line, inet_prefix) + #inet_prefix
+                local end_pos = string.find(line, " ", start_pos + 1) - 1
+                local ip_address = string.sub(line, start_pos, end_pos)
+                return cjson.encode({
+                    code = 0,
+                    type = 'offer',
+                    message = 'success',
+                    data = {
+                        address = ip_address
+                    }
+                })
             end
+        end
+    else
+        -- static
+        return cjson.encode({
+            code = 0,
+            type = 'offer',
+            message = 'success',
+            data = {
+                address = res.address
+            }
+        })
+    end
+
+    return cjson.encode({
+        code = -1,
+        type = 'offer',
+        message = 'no address found'
+    })
+end
+
+local function offer(req)
+    -- set handle offer to do something
+    return cjson.encode({
+        code = 0,
+        type = 'ack',
+        message = 'success'
+    })
+end
+
+-- params: string, response from broadcaster
+-- return: string or none
+M.on_message = function(params)
+
+    local req = cjson.decode(params)
+    if req ~= nil and req.type ~= nil and type(req.type) == 'string' then
+        if req.type == 'discovery' then -- request from agent, handle it by controller
+            return discovery(req)
+        elseif req.type == 'offer' then -- offer from controller, handle it by agent
+            return offer(req)
+        else
+            -- don't answer to other
+            return
         end
     end
 
-    return 'success'
+    return cjson.encode({
+        code = -1,
+        message = 'invalid params'
+    })
 end
 
+-- create a message to send to broadcaster
+M.load_message = function()
+    return '{"type": "discovery", "params": {"mac": "7C:27:3C:08:57:94"}}'
+end
 
 return M
